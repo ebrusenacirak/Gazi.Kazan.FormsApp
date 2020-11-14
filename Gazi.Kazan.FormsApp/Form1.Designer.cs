3cdbbfcd715145e6bacfd715dcb1d4f6514a87de
Ebru sena çırak <ebrusenacrk.61@hotmail.com>
2020-11-14 20:10:14 +0300

    Add project files.

---
+++ Gazi.Kazan.FormsApp/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Gazi.Kazan.FormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrButton = new System.Windows.Forms.Timer(this.components);
+            this.tmrSure = new System.Windows.Forms.Timer(this.components);
+            this.pnlGosterge = new System.Windows.Forms.Panel();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.pnlGosterge.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrButton
+            // 
+            this.tmrButton.Interval = 1000;
+            this.tmrButton.Tick += new System.EventHandler(this.tmrButton_Tick);
+            // 
+            // tmrSure
+            // 
+            this.tmrSure.Interval = 1000;
+            this.tmrSure.Tick += new System.EventHandler(this.tmrSure_Tick);
+            // 
+            // pnlGosterge
+            // 
+            this.pnlGosterge.BackColor = System.Drawing.Color.NavajoWhite;
+            this.pnlGosterge.Controls.Add(this.lblSkor);
+            this.pnlGosterge.Controls.Add(this.lblSure);
+            this.pnlGosterge.Location = new System.Drawing.Point(453, 1);
+            this.pnlGosterge.Name = "pnlGosterge";
+            this.pnlGosterge.Size = new System.Drawing.Size(168, 379);
+            this.pnlGosterge.TabIndex = 0;
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSkor.Location = new System.Drawing.Point(46, 88);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(51, 19);
+            this.lblSkor.TabIndex = 1;
+            this.lblSkor.Text = "Skor:";
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoSize = true;
+            this.lblSure.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSure.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSure.Location = new System.Drawing.Point(41, 35);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(2, 21);
+            this.lblSure.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 381);
+            this.Controls.Add(this.pnlGosterge);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlGosterge.ResumeLayout(false);
+            this.pnlGosterge.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrButton;
+        private System.Windows.Forms.Timer tmrSure;
+        private System.Windows.Forms.Panel pnlGosterge;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label lblSure;
+    }
+}
+
---
+++ Gazi.Kazan.FormsApp/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gazi.Kazan.FormsApp
+{
+    public partial class Form1 : Form
+    {
+        static int toplam = 0;
+        static int sure = 7;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void tmrButton_Tick(object sender, EventArgs e)
+        {
+            Button btn = new Button();
+            btn.Size = new Size(50, 50);
+            btn.Location = new Point(rnd.Next(this.ClientSize.Width - pnlGosterge.Width - btn.Width), rnd.Next(this.ClientSize.Height - btn.Height));
+            btn.Text = rnd.Next(100).ToString();
+            btn.Click += Btn_Click;
+            this.Controls.Add(btn);
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            toplam += int.Parse(btn.Text);
+            lblSkor.Text = $"Skor:{toplam}";
+            btn.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmrButton.Start();
+            tmrSure.Start();
+        }
+
+        private void tmrSure_Tick(object sender, EventArgs e)
+        {
+            sure--;
+            lblSure.Text = sure.ToString();
+            if (sure == 0)
+            {
+                tmrSure.Stop();
+                tmrButton.Stop();
+
+                DialogResult cevap = MessageBox.Show($"Skor:{toplam} \nYeniden başlamak ister misiniz?", "Oyun Bitti!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (cevap == DialogResult.Yes)
+                {
+                    sure = 7;
+                    toplam = 0;
+                    tmrSure.Start();
+                    tmrButton.Start();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+
+
+            }
+        }
+    }
+}
